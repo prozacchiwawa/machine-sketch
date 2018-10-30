fe1ad1038e9d70fde6200fb84176f626f5da04a2
arty <art.yerkes@gmail.com>
2018-10-30 14:27:02 -0700

    A powered system

--- multibody_limits2.rs
+++ multibody_limits2.rs
@@ -31,7 +31,10 @@ struct Gear {
     toothRadius: f32,
     x: f32,
     y: f32,
-    pattern: Vec<bool>
+    teeth: i16,
+    motor: f32,
+    pattern: Vec<bool>,
+    subgears: Vec<Gear>
 }
 
 #[derive(Serialize, Deserialize)]
@@ -42,15 +45,12 @@ struct Machine {
 
 const COLLIDER_MARGIN: f32 = 0.01;
 
-fn gear(
+fn create_gear(
     parent : BodyHandle,
-    num : usize,
-    innerRad : f32,
-    toothRad : f32,
-    mountAt : na::Vector2<f32>,
+    gear : &Gear,
     world : &mut World<f32>) {
-    let geom = ShapeHandle::new(Ball::new(toothRad));
-    let centerGeom = ShapeHandle::new(Ball::new(innerRad * 0.99));
+    let geom = ShapeHandle::new(Ball::new(gear.toothRadius));
+    let centerGeom = ShapeHandle::new(Ball::new(gear.bodyRadius * 0.99));
     let inertia = geom.inertia(1.0);
     let center_of_mass = geom.center_of_mass();
 
@@ -58,10 +58,16 @@ fn gear(
     gearSpin.disable_min_angle();
     gearSpin.disable_max_angle();
     
+    if (gear.motor != 0.0) {
+        gearSpin.enable_angular_motor();
+        gearSpin.set_desired_angular_motor_velocity(gear.motor);
+        gearSpin.set_max_angular_motor_torque(5000.0);
+    }
+
     let mut gearBody = world.add_multibody_link(
         parent,
         gearSpin,
-        mountAt,
+        na::Vector2::new(gear.x, gear.y),
         na::zero(),
         inertia,
         center_of_mass,
@@ -75,12 +81,18 @@ fn gear(
         Material::default(),
     );
 
-    for i in 0usize..num {
+    let teeth = gear.teeth as usize;
+    for i in 0usize..teeth {
+        // Skip based on pattern
+        if gear.pattern.len() > 0 && gear.pattern[i % gear.pattern.len()] {
+            continue
+        }
+        
         let fi = i as f32;
-        let fnum = num as f32;
+        let fnum = gear.teeth as f32;
         // Setup the other links with revolute joints.
         let angle = (fi / fnum) * PI * 2.0;
-        let dist = toothRad + innerRad;
+        let dist = gear.toothRadius + gear.bodyRadius;
         let mut r = RevoluteJoint::new(angle);
         r.enable_min_angle(angle);
         r.enable_max_angle(angle);
@@ -105,12 +117,18 @@ fn gear(
             Material::default(),
         );
     }
+
+    for i in 0usize..(gear.subgears.len()) {
+        create_gear(gearBody,&gear.subgears[i],world);
+    }
 }
 
 fn run(args : Vec<String>) -> std::result::Result<(), std::io::Error> {
     let mut file = File::open(args[1].clone())?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+
+    let machine : Machine = serde_json::de::from_str(&contents)?;
 
     /*
      * World
@@ -140,13 +158,11 @@ fn run(args : Vec<String>) -> std::result::Result<(), std::io::Error> {
     /*
      * Setup the multibody.
      */
-    let rad = 0.2;
-    let innerRad = 3.0;
-    let num = 20;
     let mut parent = BodyHandle::ground();
-    let gearAt = na::zero();
 
-    gear(parent, num, innerRad, rad, gearAt, &mut world);
+    for i in 0usize..(machine.gears.len()) {
+        create_gear(parent,&machine.gears[i],&mut world);
+    }
 
     /*
      * Set up the testbed.
